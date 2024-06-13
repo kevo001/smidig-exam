d5f63f67aba054b3fbd475c2f1f45f803c21ab4b
SK <sakr011@EGMS.no>
2024-06-13 13:39:12 +0200

    Co-authored-by: MartinABF <MartinABF@users.noreply.github.com> Co-authored-by: Jens Berger Husem <jehu004@student.kristiania.no>

--- server/db.js
+++ server/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 // MySQL Connection Pool
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'sql8.freesqldatabase.com',
-    user: 'sql8712194',
-    password: '79YJfeSGaQ',
-    database: 'sql8712194'
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7713748',
+    password: 'Aspr7rq3eB',
+    database: 'sql7713748'
 });
 
 module.exports = pool;
